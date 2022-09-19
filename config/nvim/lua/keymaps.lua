d444e62c065f09c40d613ec91e08642d8e9e9be0
whorf <whorf@whorf.dev>
2022-09-19 14:38:31 -0500

    boop

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -1,5 +1,6 @@
 local g = vim.g
 local k = vim.api.nvim_set_keymap
+local lsp = vim.lsp.buffer
 local opts = { noremap = true, silent = true }
 
 k("", "<Space>", "<Nop>", opts)
@@ -10,17 +11,17 @@ function ns(from,to)
     k("n",from,to,opts)
 end
 function nf(from,to)
-    k("n",from,to,"<cmd>lua "..tostring(opts).."()<CR>")
+    k("n",from,to,opts)
 end
 
 ns('<leader>ff', ':Telescope fd<CR>')
 ns('<leader>fG', ':Telescope live_grep<CR>')
 ns('<leader>fg', ':Telescope grep_string<CR>')
 
-nf('<leader>gd', lsp.definition)
-nf('<leader>gh', lsp.hover)
-nf('<leader>gD', lsp.implementation)
-nf('<leader>gs', lsp.signature_help)
-nf('<leader>gr', lsp.references)
-nf('<leader>gR', lsp.rename)
-nf('<leader>ga', lsp.code_action)
+nf('<leader>gd', function() lsp.definition() end)
+nf('<leader>gh', function() lsp.hover() end)
+nf('<leader>gD', function() lsp.implementation() end)
+nf('<leader>gs', function() lsp.signature_help() end)
+nf('<leader>gr', function() lsp.references() end)
+nf('<leader>gR', function() lsp.rename() end)
+nf('<leader>ga', function() lsp.code_action() end)
