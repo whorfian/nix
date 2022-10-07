3aed66f9be402e7f30ce573d4da9f5598f2d77f4
whorf <whorf@whorf.dev>
2022-10-07 13:13:54 -0500

    boop

--- config/configuration.nix
+++ config/configuration.nix
@@ -63,7 +63,7 @@
       displayManager = {
         defaultSession = "none+bspwm";
         sessionCommands = ''
-          xrandr -o left
+          #xrandr -o left
           xrandr --output HDMI-0 --mode 2560x1440 --rate 144.0
         '';
         autoLogin = {
--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.05";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     neovim.url = "github:neovim/neovim?dir=contrib";
@@ -13,7 +13,7 @@
       specialArgs = attrs // {
         user = "whorf";
         email = "whorf@whorf.dev";
-        version = "22.05";
+        version = "22.11";
         style = (import ./config/style.nix) { lib = self.lib; };
       };
       modules = [ ./config/configuration.nix ];
