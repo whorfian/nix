ddeb1c0d7294f2157ac33ef71b11d866ae34e9a5
whorf <whorf@whorf.dev>
2022-10-10 22:37:21 -0500

    boop

--- config/configuration.nix
+++ config/configuration.nix
@@ -164,13 +164,13 @@
   hardware.nvidia.modesetting.enable = true;
   hardware.opengl.enable = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-  # hardware.nvidia.optimus_prime = {
-  #   enable = true;
-  #   # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-  #   nvidiaBusId = "PCI:1:0:0";
-  #   # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-  #   intelBusId = "PCI:0:2:0";
-  # };
+  hardware.nvidia.optimus_prime = {
+    enable = true;
+    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+    nvidiaBusId = "PCI:01:00.0";
+    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+    intelBusId = "PCI:00:02.0";
+  };
 
   specialisation = {
     external-display.configuration = {
