c992aeaaa2291aca4de20533579764d43e93684c
whorf <whorf@whorf.dev>
2022-10-10 22:40:08 -0500

    boop

--- config/configuration.nix
+++ config/configuration.nix
@@ -164,12 +164,12 @@
   hardware.nvidia.modesetting.enable = true;
   hardware.opengl.enable = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-  hardware.nvidia.optimus_prime = {
-    enable = true;
+  hardware.nvidia.prime = {
+    sync.enable = true;
     # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-    nvidiaBusId = "PCI:01:00.0";
+    nvidiaBusId = "PCI:1:0:0";
     # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-    intelBusId = "PCI:00:02.0";
+    intelBusId = "PCI:2:0:0";
   };
 
   specialisation = {
