8d4c0ed4ced02890eceef315b184c9dea6c5e9e7
whorf <whorf@whorf.dev>
2022-10-10 22:33:45 -0500

    boop

--- config/configuration.nix
+++ config/configuration.nix
@@ -157,7 +157,27 @@
       (lutris.override { extraLibraries = pkgs: [ pkgs.libunwind ]; })
       dxvk
       vulkan-tools
+      pciutils
     ];
+  };
+
+  hardware.nvidia.modesetting.enable = true;
+  hardware.opengl.enable = true;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # hardware.nvidia.optimus_prime = {
+  #   enable = true;
+  #   # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+  #   nvidiaBusId = "PCI:1:0:0";
+  #   # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+  #   intelBusId = "PCI:0:2:0";
+  # };
+
+  specialisation = {
+    external-display.configuration = {
+      system.nixos.tags = [ "external-display" ];
+      hardware.nvidia.prime.offload.enable = lib.mkForce false;
+      hardware.nvidia.powerManagement.enable = lib.mkForce false;
+    };
   };
 
   programs = { steam.enable = true; };
