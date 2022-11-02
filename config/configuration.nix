493e94623efa363f1d4b151bfa4ad59e21a30e1b
whorf <whorf@whorf.dev>
2022-11-02 18:17:19 -0500

    boop

--- config/configuration.nix
+++ config/configuration.nix
@@ -123,7 +123,6 @@
   # virtualbox
   virtualisation.virtualbox.host.enable = true;
   users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
-  virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
 
   users = {
